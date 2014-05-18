d8297e6872d56d5901289b6b249d3e0c98fd76b3
mudrutom <mudrutom@gmail.com>
2014-05-18 18:29:22 +0200

    get rid of the wrapper classes

--- src/main/java/cz/au/railwaystation/domain/DomainFactory.java
+++ src/main/java/cz/au/railwaystation/domain/DomainFactory.java
@@ -1,62 +1,64 @@
 package cz.au.railwaystation.domain;
 
 import cz.au.railwaystation.fol.Factory;
+import cz.au.railwaystation.fol.Function;
+import cz.au.railwaystation.fol.Predicate;
 import cz.au.railwaystation.fol.Term;
 
 public class DomainFactory extends Factory {
 
 	protected DomainFactory() {}
 
-	public static PrAt at(Term time, Term train, Term node) {
-		return new PrAt(time, train, node);
+	public static Predicate at(Term time, Term train, Term node) {
+		return new Predicate("at", time, train, node);
 	}
 
-	public static PrEnter enter(Term time, Term train, Term node) {
-		return new PrEnter(time, train, node);
+	public static Predicate enter(Term time, Term train, Term node) {
+		return new Predicate("enter", time, train, node);
 	}
 
-	public static PrOpen open(Term time, Term node) {
-		return new PrOpen(time, node);
+	public static Predicate open(Term time, Term node) {
+		return new Predicate("open", time, node);
 	}
 
-	public static PrGoes goes(Term time, Term node) {
-		return new PrGoes(time, node);
+	public static Predicate goes(Term time, Term node) {
+		return new Predicate("goes", time, node);
 	}
 
-	public static FnSwitch swtch(Term time, Term node) {
-		return new FnSwitch(time, node);
+	public static Function swtch(Term time, Term node) {
+		return new Function("switch", time, node);
 	}
 
-	public static FnClock clock(Term time) {
-		return new FnClock(time);
+	public static Function clock(Term time) {
+		return new Function("clock", time);
 	}
 
-	public static FnGate gate(Term train) {
-		return new FnGate(train);
+	public static Function gate(Term train) {
+		return new Function("gate", train);
 	}
 
-	public static PrConf conf(Term time, Term path) {
-		return new PrConf(time, path);
+	public static Predicate conf(Term time, Term path) {
+		return new Predicate("conf", time, path);
 	}
 
-	public static PrFree free(Term time, Term path) {
-		return new PrFree(time, path);
+	public static Predicate free(Term time, Term path) {
+		return new Predicate("free", time, path);
 	}
 
-	public static PrReady ready(Term time, Term path) {
-		return new PrReady(time, path);
+	public static Predicate ready(Term time, Term path) {
+		return new Predicate("ready", time, path);
 	}
 
-	public static PrLess less(Term i, Term j) {
-		return new PrLess(i, j);
+	public static Predicate less(Term i, Term j) {
+		return new Predicate("less", i, j);
 	}
 
-	public static FnSucc succ(Term i) {
-		return new FnSucc(i);
+	public static Function succ(Term i) {
+		return new Function("succ", i);
 	}
 
-	public static FnPred pred(Term i) {
-		return new FnPred(i);
+	public static Function pred(Term i) {
+		return new Function("pred", i);
 	}
 
 }
